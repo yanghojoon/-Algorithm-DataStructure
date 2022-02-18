07fc8d0b94aade849ae8dd1d08316a82b8883420
yanghojoon <yanghojoon8487@gmail.com>
2022-02-18 11:34:22 +0900

    test: sorting 관련 테스트를 위한 파일 추가

---
+++ Stack Start/Tests/SortingTestCase.swift
@@ -0,0 +1,32 @@
+//
+//  SortingTestCase.swift
+//  DataStructuresTests
+//
+//  Created by 양호준 on 2022/02/18.
+//
+
+import XCTest
+@testable import DataStructures
+
+final class SortingTestCase: XCTestCase {
+  var testArray: [Int] = []
+  let sortedArray = [3, 4, 9, 10]
+  
+  override func setUp() {
+    testArray = [9, 4, 10, 3]
+  }
+  
+  func test_bubbleSort() {
+    bubbleSort(&testArray)
+    XCTAssertEqual(testArray, sortedArray)
+  }
+  
+  func test_selectionSort() {
+
+  }
+  
+  func test_insertionSort() {
+
+  }
+}
+
