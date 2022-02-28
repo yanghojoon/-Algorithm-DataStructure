9c5b8df671f20e1021b8e52aa32956266a960cff
yanghojoon <yanghojoon8487@gmail.com>
2022-03-01 00:19:26 +0900

    leetCode: Maximum Product Difference Between Two Pairs

---
+++ Stack Start/LeetCode/Sorting/Maximum ProductDifferenceBetweenTwoPairs.swift
@@ -0,0 +1,19 @@
+//
+//  Maximum ProductDifferenceBetweenTwoPairs.swift
+//  DataStructures
+//
+//  Created by 양호준 on 2022/03/01.
+//
+
+import Foundation
+
+class SolutionSortE {
+    func maxProductDifference(_ nums: [Int]) -> Int {
+        let sortedNumbers = nums.sorted() // 정렬하고
+        
+        let minimumPairMultiple = sortedNumbers[0] * sortedNumbers[1] // 정렬한 것에서 앞에서 2개의 요소 곱
+        let maximumPairMulitple = sortedNumbers[nums.count - 1] * sortedNumbers[nums.count - 2] // 뒤에서 2개의 요소 곱
+        
+        return maximumPairMulitple - minimumPairMultiple // 이들을 뺌
+    }
+}
