e253bdb778f2a239062fe479fc87dbea707e2ca8
yanghojoon <yanghojoon8487@gmail.com>
2022-03-29 01:10:39 +0900

    leetCode: Relative Ranks

--- Stack Start/LeetCode/Heap/LastStoneWeight.swift
+++ Stack Start/LeetCode/Heap/LastStoneWeight.swift
@@ -3,23 +3,23 @@ import Foundation
 
 class SolutionHeapB {
     func lastStoneWeight(_ stones: [Int]) -> Int {
-        var sortedStone = stones.sorted()
+        var sortedStone = stones.sorted() // 일단 돌을 무게 순으로 정렬
         
         while sortedStone.count > 2 {
-            let firstHeavyStone = sortedStone.removeLast()
-            let secondHeavyStone = sortedStone.removeLast()
+            let firstHeavyStone = sortedStone.removeLast() // 마지막에 있는게 가장 무거운 돌이기 때문에 빼고
+            let secondHeavyStone = sortedStone.removeLast() // 그 다음 무거운 돌을 뺀다.
             
-            if firstHeavyStone != secondHeavyStone {
-                let splitStone = firstHeavyStone - secondHeavyStone
-                sortedStone.append(splitStone)
-                sortedStone = sortedStone.sorted()
+            if firstHeavyStone != secondHeavyStone { // 이 둘이 같다면 그냥 더하지 않고 진행을 하기 때문에 돌의 무게가 다를 떄
+                let splitStone = firstHeavyStone - secondHeavyStone // 각 돌의 무게를 빼서 추가할 돌의 무게를 구하고
+                sortedStone.append(splitStone) // 넣은 뒤
+                sortedStone = sortedStone.sorted() // 다시 정렬
             }
         }
         
-        if sortedStone.count == 2 {
-            return sortedStone[1] - sortedStone[0]
+        if sortedStone.count == 2 { // 2개일 때에는
+            return sortedStone[1] - sortedStone[0] // 무거운 돌에서 가벼운 돌을 뺀 다음 값 반환
         }
         
-        return sortedStone[0]
+        return sortedStone[0] // 1개인 경우 sortedSort에서 마지막 남은 돌 꺼냄.
     }
 }
---
+++ Stack Start/LeetCode/Heap/RelativeRanks.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+
+class SolutionHeapC {
+    func findRelativeRanks(_ score: [Int]) -> [String] {
+        let sortedHighScore: [Int] = score.sorted().reversed() // 일단 점수를 큰 점수로 정렬, 타입 지정해줘야 함. 아니면 reversed 타입으로 인식
+        var ranks = [Int]()
+        var result = [String]()
+        
+        for myScore in score {
+            guard let rank = sortedHighScore.firstIndex(of: myScore) else { return [] }
+            
+            ranks.append(rank + 1)
+        }
+        
+        ranks.forEach { rank in
+            switch rank {
+            case 1:
+                result.append("Gold Medal")
+            case 2:
+                result.append("Silver Medal")
+            case 3:
+                result.append("Bronze Medal")
+            default:
+                result.append("\(rank)")
+            }
+        }
+        
+        return result
+    }
+}
