f0932cac362244cf20dc767337256db3a8c3c763
yanghojoon <yanghojoon8487@gmail.com>
2022-04-17 23:23:40 +0900

    programmers: 신규 아이디 추천

---
+++ Stack Start/Programmers/level1/신규 아이디 추천.swift
@@ -0,0 +1,52 @@
+import Foundation
+
+
+func solutionKakaoB(_ new_id:String) -> String {
+    var result = ""
+    // 1단계
+    result = new_id.lowercased()
+    // 2단계
+    let specialString: CharacterSet = [
+        "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "=", "+", "[", "{", "]", "}",
+        ":", "?", ",", "<", ">", "/"
+    ]
+    let filteredIDs = result.components(separatedBy: specialString)
+    result = filteredIDs.joined()
+    // 3단계
+    while result.contains("..") {
+        result = result.replacingOccurrences(of: "..", with: ".")
+    }
+    // 4단계
+    if result.count != 0 {
+        while result.first == "." {
+            result.removeFirst()
+        }
+        
+        while result.last == "." {
+            result.removeLast()
+        }
+    }
+    // 5단계
+    if result == "" {
+        result.append("a")
+    }
+    // 6단계
+    while result.count > 15 {
+        result.removeLast()
+        
+        if result.last! == "." {
+            result.removeLast()
+        }
+    }
+    
+    // 7단계
+    while result.count < 3 {
+        guard let lastWord = result.last else {
+            continue
+        }
+        
+        result.append(lastWord)
+    }
+    
+    return result
+}
