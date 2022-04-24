b5e9c0a1139326df677aa9bad12fd4941eca0175
yanghojoon <yanghojoon8487@gmail.com>
2022-04-24 12:43:47 +0900

    programmers: 다트게임

--- Stack Start/Programmers/level1/다트 게임.swift
+++ Stack Start/Programmers/level1/다트 게임.swift
@@ -1,8 +1,33 @@
 import Foundation
 
 func solution(_ dartResult:String) -> Int {
-    let letters = CharacterSet(charactersIn: "SDT*#")
-    let numbers = dartResult.components(separatedBy: letters)
+    let scores = dartResult.split(whereSeparator: { $0.isLetter || ["*", "#"].contains($0) }).compactMap { Int($0 ) }
+    let bonusesAndOptions = dartResult.split(whereSeparator: { $0.isNumber } )
+    var results = [Int]()
     
-    return 0
+    for (index, (score, bonusAndOption)) in zip(scores, bonusesAndOptions).enumerated() {
+        var result = score
+        
+        if bonusAndOption.contains("D") {
+            result *= result
+        } else if bonusAndOption.contains("T") {
+            result *= result * result
+        }
+        
+        if bonusAndOption.contains("*") {
+            result *= 2
+            
+            if index > 0 {
+                results[index - 1] *= 2
+            }
+        } else if bonusAndOption.contains("#") {
+            result *= -1
+        }
+        
+        print(result)
+        results.append(result)
+    }
+    print(results)
+    
+    return results.reduce(0, +)
 }
